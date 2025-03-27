d342b1b425b1417f3bcef8a118faf60aae966dbb
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-27 19:32:21 -0300

    fix: error in env declaration in docker-compose

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"log"
 
-func main () {
-	
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
