17fd23de60421abdc238ef684de68a6b63e60197
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-27 20:23:38 -0300

    feat: configure initial  db connection postgres

--- internal/database/database.go
+++ internal/database/database.go
@@ -13,12 +13,14 @@ var DB *gorm.DB
 
 func Connect() {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-	os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"),)
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Erro ao conectar no banco de dados:", err)
 	}
 
+	fmt.Println("Conectado ao banco de dados com sucesso!")
 	DB = db
 }
