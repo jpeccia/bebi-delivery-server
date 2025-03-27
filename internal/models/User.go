b57f3d7e879761a355ca3bd6020e78ac61589b23
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-27 20:41:17 -0300

    chore: formating user struct

--- internal/models/User.go
+++ internal/models/User.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-    Username string `json:"username"`
-    Password string `json:"password"`
-    Phone    string `json:"phone" gorm:"unique"`
-    Address  string `json:"address"`
-	Role Role `json:"role" gorm:"default:customer`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Phone    string `json:"phone" gorm:"unique"`
+	Address  string `json:"address"`
+	Role     Role   `json:"role" gorm:"default:customer"`
 }
