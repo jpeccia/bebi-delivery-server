6113791a5824078248ab0e8228d0e25c874129b0
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-27 21:16:31 -0300

    feat: create migration db file

---
+++ internal/database/migrate.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"log"
+
+	"github.com/jpeccia/bebi-delivery-server/internal/models"
+)
+
+func MigrateDB() {
+	err := DB.AutoMigrate(
+		&models.User{},
+		&models.Product{},
+		&models.Store{},
+		&models.Order{},
+		&models.OrderItem{},
+		&models.StoreProduct{},
+		&models.Rating{},
+	)
+
+	if err != nil {
+		log.Fatal("Erro ao aplicar as migrations: %v", err)
+	}
+	
+	log.Println("Migrations aplicadas com sucesso!")
+}
