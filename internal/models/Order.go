bb49a0af644dd24b824edc14b1eace858f14df78
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-27 21:03:01 -0300

    feat: create Order Model

---
+++ internal/models/Order.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type Order struct {
+	gorm.Model
+	NumPedido int `json:"num_pedido"`
+	CustomerID uint `json:"customer_id"`
+	StoreId uint `json:"store_id"`
+	Status OrderStatus `json:"status" gorm:"default:'pending'"`
+	TotalPrice float64 `json:"total_price"`
+}
