951c9a31fcd9de36cb039f4293d294d4c8caa7bb
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-27 21:09:04 -0300

    feat: create Rating Model

---
+++ internal/models/Rating.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type Rating struct {
+	gorm.Model
+	Title string `json:"title"`
+	Description string `json:"description"`
+	Rating int `json:"rating"`
+	StoreID uint `json:"store_id"`
+	UserID uint `json:"user_id"`
+}
