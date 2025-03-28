2c33701ed160eab9d6465b5c8750719279ad856f
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-27 21:07:09 -0300

    feat: create OrderItem model

---
+++ internal/models/OrderItem.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type OrderItem struct {
+	gorm.Model
+	OrderID uint `json:"order_id"`
+	ProductID uint `json:"product_id"`
+	Quantity int `json:"quantity"`
+	SubTotal float64 `json:"subtotal"`
+}
