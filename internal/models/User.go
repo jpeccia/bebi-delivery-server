9f37a5c8682717699316e230f1b114e0fca4710e
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-03-28 14:33:54 -0300

    feat: add name table in user entity db

--- internal/models/User.go
+++ internal/models/User.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Username string `json:"username"`
+	Name     string `json:"name"`
 	Password string `json:"password"`
 	Phone    string `json:"phone" gorm:"unique"`
 	Address  string `json:"address"`
