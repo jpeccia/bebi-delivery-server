66542953a537c6e52d9d515ee471a2e8f91ce21c
João Otávio Peccia <cnttspk@gmail.com>
2025-03-31 11:31:59 -0300

    feat: create CRUD functions in OrderItem Repository

--- internal/repositories/OrderItemRepository.go
+++ internal/repositories/OrderItemRepository.go
@@ -1,6 +1,9 @@
 package repositories
 
-import "github.com/jpeccia/bebi-delivery-server/internal/models"
+import (
+	"github.com/jpeccia/bebi-delivery-server/internal/models"
+	"gorm.io/gorm"
+)
 
 type OrderItemRepository interface {
 	Create(orderItem *models.OrderItem) error
@@ -9,3 +12,36 @@ type OrderItemRepository interface {
 	Update(orderItem *models.OrderItem) error
 	Delete(id uint) error
 }
+
+type orderItemRepository struct {
+	db *gorm.DB
+}
+
+func NewOrderItemRepository(db *gorm.DB) OrderItemRepository {
+	return &orderItemRepository{db}
+}
+
+func (o *orderItemRepository) Create(orderItem *models.OrderItem) error {
+	return o.db.Create(orderItem).Error
+}
+
+func (o *orderItemRepository) Delete(id uint) error {
+	return o.db.Delete(&models.OrderItem{}, id).Error
+}
+
+func (o *orderItemRepository) GetAll() ([]models.OrderItem, error) {
+	var orderItems []models.OrderItem
+	err := o.db.Find(&orderItems).Error
+	return orderItems, err
+}
+
+func (o *orderItemRepository) GetById(id uint) (*models.OrderItem, error) {
+	var orderItem models.OrderItem
+	err := o.db.Find(&orderItem).Error
+	return &orderItem, err
+}
+
+func (o *orderItemRepository) Update(orderItem *models.OrderItem) error {
+	return o.db.Save(orderItem).Error
+}
+
