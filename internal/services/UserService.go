07cddf07cbf23ea4b95b992f4ff3de202fd565c4
João Otávio Peccia <cnttspk@gmail.com>
2025-03-31 17:31:45 -0300

    fix: remove pointer unused in UserService

--- internal/services/UserService.go
+++ internal/services/UserService.go
@@ -1,12 +1,19 @@
 package services
 
-import "github.com/jpeccia/bebi-delivery-server/internal/repositories"
+import (
+	"github.com/jpeccia/bebi-delivery-server/internal/models"
+	"github.com/jpeccia/bebi-delivery-server/internal/repositories"
+)
 
 type UserService struct {
-	repo *repositories.UserRepository
+	repo repositories.UserRepository
 }
 
-func NewUserService(repo *repositories.UserRepository) *UserService {
+func NewUserService(repo repositories.UserRepository) *UserService {
 	return &UserService{repo}
 }
 
+func (s * UserService) RegisterUser(user *models.User) error {
+	user.Role = "CUSTOMER"
+	return s.repo.Create(user)
+}
