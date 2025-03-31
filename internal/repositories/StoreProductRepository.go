0160b3b3d7c0f41d956139ba3430f272e8ee9f1f
João Otávio Peccia <cnttspk@gmail.com>
2025-03-31 11:23:58 -0300

    feat: create CRUD functions in StoreProduct Repository

--- internal/repositories/StoreProductRepository.go
+++ internal/repositories/StoreProductRepository.go
@@ -1,6 +1,9 @@
 package repositories
 
-import "github.com/jpeccia/bebi-delivery-server/internal/models"
+import (
+	"github.com/jpeccia/bebi-delivery-server/internal/models"
+	"gorm.io/gorm"
+)
 
 type StoreProductRepository interface {
 	Create(storeProduct *models.StoreProduct) error
@@ -9,3 +12,36 @@ type StoreProductRepository interface {
 	Update(storeProduct *models.StoreProduct) error
 	Delete(id uint) error
 }
+
+type storeProductRepository struct {
+	db *gorm.DB
+}
+
+func NewStoreProductRepository(db *gorm.DB) StoreProductRepository {
+	return &storeProductRepository{db}
+}
+
+func (s *storeProductRepository) Create(storeProduct *models.StoreProduct) error {
+	return s.db.Create(storeProduct).Error
+}
+
+func (s *storeProductRepository) Delete(id uint) error {
+	return s.db.Delete(&models.StoreProduct{}, id).Error
+}
+
+func (s *storeProductRepository) GetAll() ([]models.StoreProduct, error) {
+	var storeProducts []models.StoreProduct
+	err := s.db.Find(&storeProducts).Error
+	return storeProducts, err
+}
+
+func (s *storeProductRepository) GetById(id uint) (*models.StoreProduct, error) {
+	var storeProduct models.StoreProduct
+	err := s.db.First(&storeProduct, id).Error
+	return &storeProduct, err
+}
+
+func (s *storeProductRepository) Update(storeProduct *models.StoreProduct) error {
+	return s.db.Save(storeProduct).Error
+}
+
