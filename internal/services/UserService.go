e171d5ca7f2964b6c297adf34389f414c3bca36e
João Otávio Peccia <cnttspk@gmail.com>
2025-03-31 17:35:31 -0300

    feat: create UpgradeToStoreOwner and Register functions in UserService

--- internal/services/UserService.go
+++ internal/services/UserService.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"errors"
+
 	"github.com/jpeccia/bebi-delivery-server/internal/models"
 	"github.com/jpeccia/bebi-delivery-server/internal/repositories"
 )
@@ -13,7 +15,20 @@ func NewUserService(repo repositories.UserRepository) *UserService {
 	return &UserService{repo}
 }
 
-func (s * UserService) RegisterUser(user *models.User) error {
+func (s *UserService) RegisterUser(user *models.User) error {
 	user.Role = "CUSTOMER"
 	return s.repo.Create(user)
+}
+
+func (s *UserService) UpgradeToStoreOwner(userID uint) error {
+	user, err := s.repo.GetById(userID)
+	if err != nil {
+		return err
+	}
+	if user.Role == "STORE_OWNER" {
+		return errors.New("usuário já é um StoreOwner")
+	}
+
+	user.Role = "STORE_OWNER"
+	return s.repo.Update(user)
 }
