1bfb3c731d5cb2c623d9a15a99e74f370c236125
João Otávio Peccia <cnttspk@gmail.com>
2025-03-31 14:31:35 -0300

    feat: add Verify Password func in Password utils

--- utils/password.go
+++ utils/password.go
@@ -5,10 +5,14 @@ import "golang.org/x/crypto/bcrypt"
 func GenerateHashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
-	if err != nil{
+	if err != nil {
 		return "", err
 	}
 
 	return string(hash), nil
 }
 
+func VerifyPassword(password, hashedPassword string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err
+}
