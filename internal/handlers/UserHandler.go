9ce74a2d97c224b9cdf8e8311f7966189c21997c
João Otávio Peccia <cnttspk@gmail.com>
2025-03-31 17:50:08 -0300

    feat: add RegisterUser function in UserHandler

--- internal/handlers/UserHandler.go
+++ internal/handlers/UserHandler.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "github.com/jpeccia/bebi-delivery-server/internal/services"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jpeccia/bebi-delivery-server/internal/models"
+	"github.com/jpeccia/bebi-delivery-server/internal/services"
+)
 
 type UserHandler struct {
 	service *services.UserService
@@ -10,3 +16,17 @@ func NewUserHandler(service *services.UserService) *UserHandler {
 	return &UserHandler{service: service}
 }
 
+func (h *UserHandler) RegisterUser(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Dados inválidos."})
+		return
+	}
+
+	if err := h.service.RegisterUser(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao registrar usuário"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Usuário registrado com sucesso"})
+}
