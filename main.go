6a978f1bf38a39306ba54b7a0bfd706cab6d4d7f
João Otávio Peccia <cnttspk@gmail.com>
2025-04-01 17:19:57 -0300

    feat: add setup routes in main and run router in main

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"log"
+
+	"github.com/gin-gonic/gin"
 	"github.com/jpeccia/bebi-delivery-server/config"
 	"github.com/jpeccia/bebi-delivery-server/internal/database"
+	"github.com/jpeccia/bebi-delivery-server/internal/handlers"
+	"github.com/jpeccia/bebi-delivery-server/internal/repositories"
+	"github.com/jpeccia/bebi-delivery-server/internal/routes"
+	"github.com/jpeccia/bebi-delivery-server/internal/services"
 )
 
 func main() {
 	config.LoadEnvVariables()
+
 	database.Connect()
 	database.MigrateDB()
+
+	userRepo := repositories.NewUserRepository(database.DB)
+
+	userService := services.NewUserService(userRepo)
+
+	userHandler := handlers.NewUserHandler(userService)
+
+	router := gin.Default()
+	routes.SetupRoutes(router, userHandler)
+
+	port := ":8080"
+	log.Printf("Servidor rodando na porta %s", port)
+	router.Run(port)
 }
