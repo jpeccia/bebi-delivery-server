7f10601627e7c6570d04941fc32351de0945432f
João Otávio Peccia <cnttspk@gmail.com>
2025-04-01 16:45:02 -0300

    feat: add UpgradeToStoreOwner function in User Handler

--- internal/handlers/UserHandler.go
+++ internal/handlers/UserHandler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jpeccia/bebi-delivery-server/internal/models"
@@ -29,4 +30,20 @@ func (h *UserHandler) RegisterUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, gin.H{"message": "Usuário registrado com sucesso"})
+}
+
+func (h *UserHandler) UpgradeToStoreOwner(c *gin.Context) {
+	userID, err := strconv.ParseUint(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID inválido"})
+		return
+	}
+
+	err = h.service.UpgradeToStoreOwner(uint(userID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Usuário agora é um StoreOwner"})
 }
