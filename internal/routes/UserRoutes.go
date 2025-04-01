57656e2fa96a6ac0b7e59ca154265680c18ed5df
João Otávio Peccia <cnttspk@gmail.com>
2025-04-01 17:00:01 -0300

    feat: add login POST route and add middleware in upgrade to store owner in UserRoutes

--- internal/routes/UserRoutes.go
+++ internal/routes/UserRoutes.go
@@ -3,12 +3,14 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jpeccia/bebi-delivery-server/internal/handlers"
+	"github.com/jpeccia/bebi-delivery-server/internal/middlewares"
 )
 
 func UserRoutes(router *gin.Engine, userHandler *handlers.UserHandler) {
 	userGroup := router.Group("/users")
 	{
 		userGroup.POST("/register", userHandler.RegisterUser)
-		userGroup.PUT("/:id/upgrade", userHandler.UpgradeToStoreOwner)
+		userGroup.POST("/login", userHandler.Login)
+		userGroup.PUT("/:id/upgrade", middlewares.AuthMiddleware(), userHandler.UpgradeToStoreOwner)
 	}
 }
