6e8e339aba12bf5fe8425d4edab3dd6bf75da7b0
João Otávio Peccia <cnttspk@gmail.com>
2025-04-09 11:51:39 -0300

    feat: add isblocked in Store model

--- internal/models/Store.go
+++ internal/models/Store.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type Store struct {
 	gorm.Model
-	Name    string `json:"name"`
-	OwnerID uint   `json:"owner_id"`
-	Phone   string `json:"phone"`
-	Email   string `json:"email"`
-	Rating  int    `json:"rating"`
-	Address string `json:"address"`
+	Name      string `json:"name"`
+	OwnerID   uint   `json:"owner_id"`
+	Phone     string `json:"phone"`
+	Email     string `json:"email"`
+	Rating    int    `json:"rating"`
+	Address   string `json:"address"`
+	IsBlocked bool   `gorm:"default:false" json:"is_blocked"`
 }
